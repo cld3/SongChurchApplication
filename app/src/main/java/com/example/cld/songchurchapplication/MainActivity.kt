dfda20746b19606dfa30c4c49453711fd2589944
cld <cld_@outlook.com>
2018-08-16 16:51:34 +0300

    Добавленна зависимость для FirebaseFirestore

--- app/src/main/java/com/example/cld/songchurchapplication/MainActivity.kt
+++ app/src/main/java/com/example/cld/songchurchapplication/MainActivity.kt
@@ -4,13 +4,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cld.songchurchapplication.signIn.SignInActivity
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        FirebaseFirestore.getInstance()
         startActivity(Intent(this, SignInActivity::class.java))
     }
 }
